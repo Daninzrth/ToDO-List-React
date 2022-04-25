925b6e17d30fdeb82599ad6f231c53c1a2d20544
Daninzrth <dani.angulo1910@gmail.com>
2022-04-25 20:40:11 +0000

    Listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,71 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
+	const [tarea, addTarea] = useState([]);
+	const [input, setInput] = useState("");
+	useEffect(() => console.log(tarea), [tarea]);
+	function borrar(i) {
+		let newArray = tarea.filter((valor, index) => {
+			return index !== i;
+		});
+		addTarea(newArray);
+	}
+	const Pendientes = () => {
+		let cantidad = tarea.length;
+		if (cantidad == 0) {
+			return <p>No tienes tareas pendientes, buen trabajo!</p>;
+		} else {
+			return (
+				<p>
+					Tienes <b>{cantidad}</b> tareas por hacer!
+				</p>
+			);
+		}
+	};
+
+	const tareas = tarea.map((unaTarea, i) => {
+		return (
+			<li key={i} className="">
+				<span>
+					<button
+						onClick={() => {
+							borrar(i);
+						}}>
+						<i className="far fa-trash-alt" />
+					</button>
+				</span>
+				{unaTarea}
+			</li>
+		);
+	});
+	function handleKeyPress(e) {
+		if (e.key === "Enter") {
+			addTarea([...tarea, input]);
+			setInput("");
+		}
+	}
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container todo">
+			<div className="row">
+				<h1>ToDo List</h1>
+			</div>
+			<div className="row">
+				<input
+					type="text"
+					placeholder="Agregar nueva tarea"
+					onKeyPress={handleKeyPress}
+					onChange={(e) => setInput(e.target.value)}
+					value={input}
+				/>
+			</div>
+			<div>
+				<ul>{tareas}</ul>
+			</div>
+			<div className="pendientes">
+				<Pendientes />
+			</div>
 		</div>
 	);
 };
